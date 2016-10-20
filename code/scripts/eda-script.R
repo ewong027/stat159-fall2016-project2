1845c7f3ba56cd4d70748915e7348cb80b3b9612
Erica Wong <ewong027@berkeley.edu>
2016-10-20 00:25:38 -0700

    code: Updating eda-script.R to include qualitative variable analysis

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -22,9 +22,39 @@ range <- c(max(credit$Income)-min(credit$Income),
            max(credit$Balance)-min(credit$Balance))
 data_frame <- data.frame (categories, sd, iqr, range)
 
-#Producing Quantitative Variable Comparison Output
+# Making Qualitative Variable Information Tables
+# Gender
+gender_freq <- table(credit$Gender)
+gender_rel_freq <- gender_freq/nrow(credit)
+gender_df <- data.frame(gender_freq, gender_rel_freq)
+gender_df$Var1.1 <- NULL
+colnames(gender_df) <- c('Gender', 'Frequency', 'Relative Frequency')
+
+# Student
+student_freq <- table(credit$Student)
+student_rel_freq <- student_freq/nrow(credit)
+student_df <- data.frame(student_freq, student_rel_freq)
+student_df$Var1.1 <- NULL
+colnames(student_df) <- c('Student?', 'Frequency', 'Relative Frequency')
+
+# Married
+married_freq <- table(credit$Married)
+married_rel_freq <- married_freq/nrow(credit)
+married_df <- data.frame(married_freq, married_rel_freq)
+married_df$Var1.1 <- NULL
+colnames(married_df) <- c('Married?', 'Frequency', 'Relative Frequency')
+
+# Ethnicity
+ethnicity_freq <- table(credit$Ethnicity)
+ethnicity_rel_freq <- ethnicity_freq/nrow(credit)
+ethnicity_df <- data.frame(ethnicity_freq, ethnicity_rel_freq)
+ethnicity_df$Var1.1 <- NULL
+colnames(ethnicity_df) <- c('Ethnicity', 'Frequency', 'Relative Frequency')
+
+#Producing Eda Output File
 sink('data/eda-output.txt')
-cat('Summary of Income\n')
+cat('Quantitative Variable Information\n')
+cat('\nSummary of Income\n')
 summary(credit$Income)
 cat('\nSummary of Limit\n')
 summary(credit$Limit)
@@ -40,6 +70,15 @@ cat('\nSummary of Balance\n')
 summary(credit$Balance)
 cat('\nSummary of Other Descriptive Statistics\n')
 print(data_frame)
+cat('\n\nQualitative Variable Tables\n')
+cat('\nGender Table\n')
+print(gender_df)
+cat('\nStudent Table\n')
+print(student_df)
+cat('\nMarried Table\n')
+print(married_df)
+cat('\nEthnicity Table\n')
+print(ethnicity_df)
 sink()
 
 # Histograms for Quantitative Variables
@@ -134,5 +173,28 @@ dev.off()
 png('images/boxplot-balance.png')
 boxplot(credit$Balance, ylab = 'Frequency', main = 'Boxplot of Balance')
 dev.off()
+
+# Barcharts for Qualitative Variables
+png('images/barchart-gender.png')
+barplot(table(credit$Gender), ylab = 'Count', xlab = 'Gender',
+        col = '#f6ef40')
+dev.off()
+
+png('images/barchart-student.png')
+barplot(table(credit$Student), ylab = 'Count', xlab = 'Student?',
+        col = '#6521d0')
+dev.off()
+
+png('images/barchart-married.png')
+barplot(table(credit$Married), ylab = 'Count', xlab = 'Married?',
+        col = '#f40e82')
+dev.off()
+
+png('images/barchart-ethnicity.png')
+barplot(table(credit$Ethnicity), ylab = 'Count', xlab = 'Ethnicity',
+        col = '#49f277', ylim = c(0, 250))
+dev.off()
+
+
 
 
