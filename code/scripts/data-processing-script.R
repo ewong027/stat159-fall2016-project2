2a6abe71b3560c0dcd16ac6514d29b8005014736
Bryana Gutierrez <bygutierrez@berkeley.edu>
2016-10-23 17:26:11 -0700

    scripts: changing the scaled-credit.csv data set so that it doesn't have a counter column

--- code/scripts/data-processing-script.R
+++ code/scripts/data-processing-script.R
@@ -4,11 +4,14 @@ credit <- read.csv("data/Credit.csv", header = TRUE)
 #dummy out categorical variables
 temp_credit <- model.matrix(Balance ~ ., data = credit)
 
-# removing column of ones, and appending Balance
+#removing column of ones, and appending Balance
 new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
 
-# scaling and centering
-scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
+#scaling and centering
+scaled_credit_pre <- scale(new_credit, center = TRUE, scale = TRUE)
 
-# export scaled data
+#removing the first column which is just a scaled counter
+scaled_credit <- scaled_credit_pre[ ,-1]
+
+#export scaled data
 write.csv(scaled_credit, file = "data/scaled-credit.csv")
