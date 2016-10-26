ad8ba38b2df9dc02050bbace1b341363fb8d93c9
Erica Wong <ewong027@berkeley.edu>
2016-10-26 13:01:23 -0700

    scripts: Updating the ols regression script to save the output to a Rdata file

--- code/scripts/ols-regression-script.R
+++ code/scripts/ols-regression-script.R
@@ -1,6 +1,8 @@
 #Loading in the data. 
-
 scaled_credit <- read.csv("data/datasets/scaled-credit.csv", header = TRUE)
 
 #Creating the OLS regression model. 
 ols_reg <- lm(Balance ~ ., data = scaled_credit)
+
+#Saving Data
+save(ols_reg, file = 'data/RData-files/ols-regression.RData')
