0393f0c54b3de87fb3a8bf1385eaecb280a262aa
Erica Wong <ewong027@berkeley.edu>
2016-10-26 13:16:50 -0700

    scripts: adding session info script to project 2

---
+++ code/scripts/session-info-script.R
@@ -0,0 +1,17 @@
+# Loading Packages that are Used 
+library(ggplot2)
+library(xtable)
+library(glmnet)
+library(pls)
+
+# Generating Session-Info.txt
+sink('session-info.txt')
+cat('General Information about R\n\n')
+sessionInfo()
+cat('\nVerison Nickname\n')
+version$nickname
+cat('\nLicense Relating to R\n')
+license()
+cat('Packages that are used\n')
+cat('ggplot2, xtable, glmnet, pls')
+sink()
