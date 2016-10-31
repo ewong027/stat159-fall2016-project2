d92b3b5793d4183484698093b47fe05d5156d070
Bryana Gutierrez <bygutierrez@berkeley.edu>
2016-10-30 23:17:35 -0700

    code: adding the calculation of MSE for the OLS analysis. Then subsequently saved the output in the RData file ols-regression.RData

--- code/scripts/ols-regression-script.R
+++ code/scripts/ols-regression-script.R
@@ -4,5 +4,8 @@ scaled_credit <- read.csv("data/datasets/scaled-credit.csv", header = TRUE)
 #Creating the OLS regression model. 
 ols_reg <- lm(Balance ~ ., data = scaled_credit)
 
+#Calculating MSE 
+ols_mse <- mean(ols_reg$residuals^2)
+
 #Saving Data
-save(ols_reg, file = 'data/RData-files/ols-regression.RData')
+save(ols_reg, ols_mse, file = 'data/RData-files/ols-regression.RData')
