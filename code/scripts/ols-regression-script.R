d081422f276d219d2a322896fc5c104c14823649
Erica Wong <ewong027@berkeley.edu>
2016-11-01 14:24:30 -0700

    scripts: updating OLS script with correct MSE calculation

--- code/scripts/ols-regression-script.R
+++ code/scripts/ols-regression-script.R
@@ -6,8 +6,6 @@ scaled_credit$X <- NULL
 ols_reg <- lm(Balance ~ ., data = train_set)
 
 #Calculating MSE 
-ols_mse <- mean(ols_reg$residuals^2)
-
 ols_matrix_new <- test_set[ ,-12]
 ols_preditctions <- predict(ols_reg, ols_matrix_new)
 ols_MSE <- mean((ols_preditctions-test_set$Balance)^2)
@@ -18,11 +16,11 @@ ols_summary <- summary(lm(Balance ~ ., data = scaled_credit))
 #Generating OLS Output
 sink('data/outputs/ols-regression-output.txt')
 cat('MSE of OLS Regression\n')
-print(ols_mse)
+print(ols_MSE)
 cat('\nCoefficients of Full Model using OLS Regression\n')
 print(ols_summary$coefficients[ ,1])
 sink()
 
 
 #Saving Data
-save(ols_reg, ols_summary, ols_mse, file = 'data/RData-files/ols-regression.RData')
+save(ols_reg, ols_summary, ols_MSE, file = 'data/RData-files/ols-regression.RData')
